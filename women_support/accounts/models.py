874dfdee2f9cd5dee5806f70d550081f4e1cc4b2
YoungTeppa <teppaxavi@gmail.com>
2025-07-15 14:24:11 +0100

    inserting a role field on registration form model

--- women_support/accounts/models.py
+++ women_support/accounts/models.py
@@ -26,7 +26,8 @@ class User(AbstractUser):
     country = models.CharField(max_length=100)
     birthdate = models.DateField(null=True, blank=True)
     is_approved = models.BooleanField(default=True)
-
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, blank=True, null=True)
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
